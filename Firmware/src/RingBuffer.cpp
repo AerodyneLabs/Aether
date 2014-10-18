fc192b0f5ce7ee96c714792f5d82bf38fb8f6830
Ethan Harstad <ethanharstad@gmail.com>
2014-10-17 21:55:29 -0500

    Document RingBuffer implementation

--- Firmware/src/RingBuffer.cpp
+++ Firmware/src/RingBuffer.cpp
@@ -1,13 +1,17 @@
 #include "RingBuffer.h"
 
 RingBuffer::RingBuffer(const uint32_t len) {
+	// Store buffer length
 	length = len;
+	// Create byte buffer
 	buf = new uint8_t[length];
+	// Initialize head and count
 	head = 0;
 	count = 0;
 }
 
 void RingBuffer::empty(void) {
+	// Reinitialize head and count
 	head = 0;
 	count = 0;
 }
@@ -17,9 +21,13 @@ bool RingBuffer::isEmpty(void) {
 }
 
 uint8_t RingBuffer::read(void) {
+	// Store byte from buffer head
 	uint8_t x = buf[head];
+	// Increment buffer head, wrapping appropriately
 	head = (head + 1) % length;
+	// Update element count
 	count--;
+	// Return read byte
 	return x;
 }
 
@@ -28,12 +36,16 @@ bool RingBuffer::isFull(void) {
 }
 
 void RingBuffer::write(const uint8_t x) {
+	// Compute the index of the buffer tail, wrapping appropriately
 	uint32_t tail = (head + count) % length;
+	// Store the input at the tail
 	buf[tail] = x;
+	// Update element count
 	if(count == length) {
 		// Buffer full, overwrite first element
 		head = (head + 1) % length;
 	} else {
+		// Buffer not full, increment count
 		count++;
 	}
 }
